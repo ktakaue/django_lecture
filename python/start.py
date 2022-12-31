e74b0042d92f19fa66e228ac0c321fdbeafca48a
keita.takaue <keita.takaue@gmail.com>
2022-12-31 18:05:51 +0900

    文字列、数値、if文まで

---
+++ python/start.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+# Your code here!
+
+# print("こんにちは")
+
+# kevin = 1
+# keita = 10
+
+# print(f"kevin{keita}"+f"keita{1}")
+
+print(1 != 1)
+score = 10
+if score > 50:
+    score = 100
+    print(score)
+elif score > 30:
+    print('ドンマイ')
+else:
+    print('異常者')
