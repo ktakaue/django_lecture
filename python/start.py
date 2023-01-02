68a18edabf75dbcb6369fda36b10b7eb21047423
keita.takaue <keita.takaue@gmail.com>
2023-01-02 17:05:05 +0900

    標準入出力の追加

--- python/start.py
+++ python/start.py
@@ -8,12 +8,5 @@
 
 # print(f"kevin{keita}"+f"keita{1}")
 
-print(1 != 1)
-score = 10
-if score > 50:
-    score = 100
-    print(score)
-elif score > 30:
-    print('ドンマイ')
-else:
-    print('異常者')
+name = input('君は誰？')
+print(name);
